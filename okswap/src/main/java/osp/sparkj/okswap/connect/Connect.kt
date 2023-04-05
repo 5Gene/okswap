e42c0b2f7baacead5666483a417857c3e28b17b2
ZuYun <jonas.jzy@gmail.com>
2023-04-05 19:57:27 +0800

    ☀️

--- okswap/src/main/java/osp/sparkj/okswap/connect/Connect.kt
+++ okswap/src/main/java/osp/sparkj/okswap/connect/Connect.kt
@@ -3,18 +3,18 @@ package osp.sparkj.okswap.connect
 /**
  * @author yun.
  * @date 2023/3/17
- * @des [一句话描述]
+ * @des [送信要 先通过驿丞 找到 驿站 驿站提供 信鸽 不同信件 不同信鸽 起飞 送信 国省市县]
  * @since [https://github.com/5hmlA]
  * <p><a href="https://github.com/5hmlA">github</a>
  */
 
-interface Nest {
-    fun handle(pigeon: Pigeon)
+interface Nest<LETTER> {
+    fun handle(pigeon: Pigeon<LETTER>)
 }
 
 
-interface Pigeon {
-    fun fly(byteArray: ByteArray)
+interface Pigeon<LETTER> {
+    fun fly(LETTER: LETTER)
 }
 
 interface Address {}
@@ -23,8 +23,22 @@ interface Address {}
  * 驿站
  * 两个驿站之间训练出信鸽
  */
-interface Relay<ADDR : Address> {
+interface Relay<ADDR : Address, LETTER> {
 
     var address: ADDR
-    suspend fun train(peer: Relay<ADDR>): Pigeon
+
+    suspend fun train(peer: Relay<ADDR, LETTER>): Pigeon<LETTER>
+
+    fun pigeon(LETTER: LETTER): Pigeon<LETTER>
+
+    fun discard()
+}
+
+/**
+ * 驿丞
+ */
+object RelayDeputy {
+//    fun <ADDR : Address, LETTER> findRelay(address: Address): Relay<ADDR, LETTER> {
+//        //todo
+//    }
 }
